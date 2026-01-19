9b9c8924db9da65b7a23a0c21f2e0c619836886e
Hafidz <hafidz.al@mhs.itenas.ac.id>
2026-01-19 11:22:32 +0700

    mengkoneksikan db ke railway

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 import mysql from "mysql2";
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_rentalps",
+import mysql from "mysql2";
+
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
+
 
 db.connect((err) => {
   if (err) {

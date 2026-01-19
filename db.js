7e7adad0c6d43c76f40885c4736dba9d3f412957
Hafidz <hafidz.al@mhs.itenas.ac.id>
2026-01-19 21:53:41 +0700

    koreksi di db.js

--- db.js
+++ db.js
@@ -1,19 +1,15 @@
 import mysql from "mysql2";
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   port: process.env.MYSQLPORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("DB ERROR:", err);
-  } else {
-    console.log("DB CONNECTED");
-  }
-});
-
-export default db;
+export default pool;
